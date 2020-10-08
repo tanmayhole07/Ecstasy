5c9059c64ba88b2a67b6fe825348a27c309fcdb3
Tanmay Hole <tanmayhole99@gmail.com>
2020-10-08 21:30:58 +0530

    Update : Group List - PopupMenu Complete ThereProfileActivity.java Not working properly Some issues when user changes his/her name dose'nt get updated in posts properly Chat Notification Dose'nt work out
    Rest All Activities working correctly

--- app/src/main/java/com/example/socialm/DashboardActivity.java
+++ app/src/main/java/com/example/socialm/DashboardActivity.java
@@ -2,10 +2,15 @@ package com.example.socialm;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.PopupMenu;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -29,6 +34,8 @@ public class DashboardActivity extends AppCompatActivity {
     String mUID = "uid";
     ActionBar actionBar;
 
+    private BottomNavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +44,7 @@ public class DashboardActivity extends AppCompatActivity {
         actionBar = getSupportActionBar();
         firebaseAuth = FirebaseAuth.getInstance();
 
-        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView = findViewById(R.id.navigation);
         navigationView.setOnNavigationItemSelectedListener(selectedListener);
 
         actionBar.setTitle("Home");
@@ -59,6 +66,7 @@ public class DashboardActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener selectedListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @RequiresApi(api = Build.VERSION_CODES.KITKAT)
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
@@ -93,17 +101,46 @@ public class DashboardActivity extends AppCompatActivity {
                             ft4.commit();
                             return true;
 
-                        case R.id.nav_notification:
-                            actionBar.setTitle("Notifications");
-                            NotificationFragment fragment5 = new NotificationFragment();
-                            FragmentTransaction ft5 = getSupportFragmentManager().beginTransaction();
-                            ft5.replace(R.id.content, fragment5, "");
-                            ft5.commit();
+                        case R.id.nav_more:
+                            showMoreOptions();
                             return true;
                     }
                     return false;
                 }
             };
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void showMoreOptions() {
+
+        PopupMenu popupMenu = new PopupMenu(this, navigationView, Gravity.END);
+
+        popupMenu.getMenu().add(Menu.NONE, 0, 0, "Notifications");
+        popupMenu.getMenu().add(Menu.NONE, 1, 0, "Group Chats");
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id==0){
+                    actionBar.setTitle("Notifications");
+                    NotificationFragment fragment5 = new NotificationFragment();
+                    FragmentTransaction ft5 = getSupportFragmentManager().beginTransaction();
+                    ft5.replace(R.id.content, fragment5, "");
+                    ft5.commit();
+
+                }else if (id==1){
+                    actionBar.setTitle("Group Chat");
+                    GroupChatFragment fragment6 = new GroupChatFragment();
+                    FragmentTransaction ft6 = getSupportFragmentManager().beginTransaction();
+                    ft6.replace(R.id.content, fragment6, "");
+                    ft6.commit();
+
+                }
+                return false;
+            }
+        });
+        popupMenu.show();
+    }
 
 
     @Override
